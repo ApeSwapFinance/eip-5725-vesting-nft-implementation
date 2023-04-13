bbf82089c471577215f3e40fadc4cfe4df96fd3a
defifofum <defifofum0@gmail.com>
2023-04-13 16:49:10 -0400

    fix: Export helper functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-/**
- * The src/ directory is where files can be written, built and published as an npm package to support
- *  the smart contracts developed in this repo.
- */
-const moduleState = undefined
-export { moduleState }
+export * from './erc5725'
