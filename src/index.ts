619fd951d9134624b29463683fa596e6425e90ce
defifofum <defifofum0@gmail.com>
2023-04-13 19:36:55 -0400

    refactor: Organize package structure, export artifacts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './erc5725'
 export { ERC5725, ERC5725__factory, IERC5725, IERC5725__factory } from '../typechain-types'
+import ERC5725_Artifact from '../artifacts/contracts/ERC5725.sol/ERC5725.json';
+import IERC5725_Artifact from '../artifacts/contracts/IERC5725.sol/IERC5725.json';
+export { ERC5725_Artifact, IERC5725_Artifact}
